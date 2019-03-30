e2c41fb7699d19aca97eb7a941851419a32ac2c5
erkankrcr <erkan_krcr_1998@outlook.com.tr>
2019-03-30 13:03:40 +0300

    Şifremi Unuttum buttonu çalışıyormu diye Toast mesaj eklendi Fragmentin içinde bulunan geri butonuna Intent Eklenerek Login Screen e geri dönüş sağlandı

--- app/src/main/java/com/example/sendtivity/LoginFragment.java
+++ app/src/main/java/com/example/sendtivity/LoginFragment.java
@@ -2,11 +2,12 @@ package com.example.sendtivity;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Toast;
 
 
 public class LoginFragment extends Fragment {
@@ -18,6 +19,20 @@ public class LoginFragment extends Fragment {
         System.out.println("Fragmente Ulaştı");
 
        View view =  inflater.inflate(R.layout.fragment_login, container, false);
+       view.findViewById(R.id.LoginRecoBtn).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getActivity(),"Şifreni unuttun buttonuna basıldı",Toast.LENGTH_SHORT).show();
+           }
+       });
+       view.findViewById(R.id.LoginBackBtn).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(getActivity(),LoginScreen.class);
+               startActivity(intent);
+
+           }
+       });
 
        return view;
     }
